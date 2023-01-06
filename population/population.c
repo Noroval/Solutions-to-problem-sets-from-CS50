c3668dcd6a2ade8e9d819fd49702cf94b45be3da
Noroval <122039895+Noroval@users.noreply.github.com>
2023-01-06 17:32:26 +0000

    Fri, Jan 6, 2023, 5:32 PM +00:00

--- population/population.c
+++ population/population.c
@@ -11,10 +11,25 @@ int main(void)
     }
     while (start < 9);
 
-
     // TODO: Prompt for end size
+    int end;
+    do
+    {
+        end = get_int("End size: ");
+    }
+    while (end <= start);
+
+    //Keep track of number of years
+    int years = 0;
 
     // TODO: Calculate number of years until we reach threshold
+    while (start < end)
+    {
+        start = start + (start / 3) - (start / 4);
+        years++;
+    }
 
     // TODO: Print number of years
+    printf("Years: %i\n", years);
+
 }
