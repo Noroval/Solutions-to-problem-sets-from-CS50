dd9a5580ff4d529592e8e32e2644f0c8de76e648
Noroval <122039895+Noroval@users.noreply.github.com>
2023-01-06 16:35:34 +0000

    Fri, Jan 6, 2023, 4:35 PM +00:00

--- population/population.c
+++ population/population.c
@@ -22,6 +22,13 @@ int main(void)
     //Keep track of number of years
     int years = 0;
 
+    //Update population
+    while (start < end)
+    {
+         start = start + (start / 3) - (start / 4);
+         years++;
+    }
+
     // TODO: Calculate number of years until we reach threshold
     n = n + (n / 3) - (n / 4);
 
