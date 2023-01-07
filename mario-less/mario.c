abe9072c0d048ff1065bc5601129f012100cf354
Noroval <122039895+Noroval@users.noreply.github.com>
2023-01-07 12:46:40 +0000

    Sat, Jan 7, 2023, 12:46 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -6,6 +6,7 @@ int main(void)
     int height;
     int row;
     int column;
+    int space;
     do
     {
         height = get_int ("Height: ");
@@ -14,6 +15,10 @@ int main(void)
 
     for (row = 0; row < height; row++)
     {
+        for (space = 0; space < space - row -1; space++)
+        {
+            printf(" ");
+        }
         for (column = 0; column <= row; column++)
         {
             printf("#");
