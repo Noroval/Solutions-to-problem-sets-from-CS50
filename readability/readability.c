846ade3ecdafce7b6d43025280f05313d70cb2b8
Noroval <122039895+Noroval@users.noreply.github.com>
2023-01-11 20:37:16 +0000

    Wed, Jan 11, 2023, 8:37 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,11 +1,33 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
 
 int main(void)
 {
-    int letters;
-    int words;
-    int sentences;
-    
+    int letters = 0;
+    int words = 0;
+    int sentences = 0;
+
+    string text = get_string("Text: ");
+
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if (isalpha(text[i]))
+        {
+            letters++;
+        }
+        else if (text[i] == " ")
+        {
+            words++;
+        }
+        else if (text[i] == "." || "!" || "?")
+        {
+            sentences++;
+        }
+    }
+
+
 
 }
