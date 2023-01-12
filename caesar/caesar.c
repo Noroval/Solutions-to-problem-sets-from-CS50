8f334701e2a08d8d1b97b4a695abe990fad13181
Noroval <122039895+Noroval@users.noreply.github.com>
2023-01-12 19:29:55 +0000

    Thu, Jan 12, 2023, 7:29 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -18,14 +18,7 @@ int main(int argc, string argv[])
     }
     //checks if it's digit or not
 
-    for (int i = 0; i < strlen(argv[1]); i++)
-    {
-        if (isdigit(argv[1][i]))
-        {
-            printf("Usage: ./caesar key\n");
-            return 1;
-        }
-    }
+
         //converts string to an int
 
         int key = atoi(argv[1]);
@@ -53,5 +46,14 @@ int main(int argc, string argv[])
             }
         }
         printf("\n");
+    }
+    bool only_digits(string s)
+    for (int i = 0; i < strlen(argv[1]); i++)
+    {
+        if (isdigit(argv[1][i]))
+        {
+            printf("Usage: ./caesar key\n");
+            return 1;
+        }
     }
 
