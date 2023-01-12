1c70d88927db80176e0e7604c9cdd28c7adf0526
Noroval <122039895+Noroval@users.noreply.github.com>
2023-01-12 19:41:38 +0000

    Thu, Jan 12, 2023, 7:41 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -10,41 +10,38 @@ int main(int argc, string argv[])
 
 {
     //checks if it's one argument
-
     if (argc != 2)
     {
         printf("Input one argument!\n");
         return 1;
     }
+
     //checks if it's digit or not
     bool only_digits(argv[1]);
 
-        //converts string to an int
+    //converts string to an int
+    int key = atoi(argv[1]);
 
-        int key = atoi(argv[1]);
+    //asks user for text
+    string plaintext = get_string("plaintext: ");
+    printf("ciphertext: ");
 
-        //asks user for text
-
-        string plaintext = get_string("plaintext: ");
-        printf("ciphertext: ");
-
-        //for every character in text encrypts it according to argument input
-
-        for (int j = 0; j < strlen(plaintext); j++)
+    //for every character in text encrypts it according to argument input
+    for (int j = 0; j < strlen(plaintext); j++)
+    {
+        if (isupper(plaintext[j]))
         {
-            if (isupper(plaintext[j]))
-            {
-                printf("%c", (plaintext[j] -65 + key) % 26 + 65);
-            }
-            else if (islower(plaintext[j]))
-            {
-                printf("%c", (plaintext[j] -97 + key) % 26 + 97);
-            }
-            else
-            {
-                printf("%c", plaintext[j]);
-            }
+            printf("%c", (plaintext[j] -65 + key) % 26 + 65);
         }
+        else if (islower(plaintext[j]))
+        {
+            printf("%c", (plaintext[j] -97 + key) % 26 + 97);
+        }
+        else
+        {
+            printf("%c", plaintext[j]);
+        }
+    }
         printf("\n");
     }
 
