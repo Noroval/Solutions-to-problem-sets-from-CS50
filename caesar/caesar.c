47f2afc3e83630107f464070bd844fb0a441c41a
Noroval <122039895+Noroval@users.noreply.github.com>
2023-01-12 11:03:06 +0000

    Thu, Jan 12, 2023, 11:03 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,6 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(int argc, string argv[])
 {
@@ -11,4 +12,7 @@ int main(int argc, string argv[])
         return 1;
     }
     for (int i = 0; i < strlen(argv[1]; i++))
+    {
+        if 
+    }
 }
