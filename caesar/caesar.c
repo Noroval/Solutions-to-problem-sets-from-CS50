e515a3ce309f715463a3463cbb75f3ec0dd30232
Noroval <122039895+Noroval@users.noreply.github.com>
2023-01-12 20:20:48 +0000

    Thu, Jan 12, 2023, 8:20 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -45,15 +45,15 @@ int main(int argc, string argv[])
         printf("\n");
     }
 
-    bool only_digits(string s)
+bool only_digits(string s)
+{
+    for (int i = 0; i < strlen(argv[1]); i++)
     {
-        for (int i = 0; i < strlen(argv[1]); i++)
+        if (!isdigit(argv[1][i]))
         {
-            if (!isdigit(argv[1][i]))
-            {
-                printf("Usage: ./caesar key\n");
-                return 1;
-            }
+            printf("Usage: ./caesar key\n");
+            return 1;
         }
     }
-   
+}
+
