39e91dce3f300d2d3d62cb54541100d1a1606c56
Noroval <122039895+Noroval@users.noreply.github.com>
2023-01-13 12:12:57 +0000

    Fri, Jan 13, 2023, 12:12 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -15,6 +15,7 @@ int main(int argc, string argv[])
         printf("Usage: ./caesar key\n");
         return 1;
     }
+    return 0;
 
     //checks if it's digit or not
     bool only_digits(string s);
