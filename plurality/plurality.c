c7ac28a537939b46cfd37bfc816063bf7e2149be
Noroval <122039895+Noroval@users.noreply.github.com>
2023-01-18 15:44:21 +0000

    Wed, Jan 18, 2023, 3:44 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -80,6 +80,15 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    // TODO
+    int maxvotes;
+
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes > maxvotes)
+        {
+            maxvotes = candidates[i].votes;
+        }
+    }
+    
     return;
 }
