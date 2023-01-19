048d319a049696a4b27597948f39638d798eb4fc
Noroval <122039895+Noroval@users.noreply.github.com>
2023-01-19 13:52:44 +0000

    Thu, Jan 19, 2023, 1:52 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -173,10 +173,16 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-    int min = MAX_VOTERS
+    int min = MAX_VOTERS;
+
     for (int i = 0; i < candidate_count; i++)
-    
-    return 0;
+    {
+        if (candidates[i].eliminated == false && candidates[i].votes < min)
+        {
+            min = candidates[i].votes;
+        }
+    }
+    return min;
 }
 
 // Return true if the election is tied between all candidates, false otherwise
