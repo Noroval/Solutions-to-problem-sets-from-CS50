bde0be75ccef95fa7ed210293c1f1aa26b40b367
Noroval <122039895+Noroval@users.noreply.github.com>
2023-01-19 11:59:42 +0000

    Thu, Jan 19, 2023, 11:59 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -147,7 +147,9 @@ void tabulate(void)
         for (int j = 0; j < candidate_count; j++)
         {
             if (candidates[preferences[i][j]].eliminated == false)
-            
+            {
+                candidates[preferences[i][j]].votes++;
+            }
         }
     }
     return;
