fc508bcc4c9974055829e398d87469573ca7a0f0
Noroval <122039895+Noroval@users.noreply.github.com>
2023-01-19 13:35:07 +0000

    Thu, Jan 19, 2023, 1:35 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -159,7 +159,8 @@ void tabulate(void)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
-    int majority = 
+    int majority = (voter_count / 2);
+    
     return false;
 }
 
