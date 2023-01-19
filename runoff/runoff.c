ddd420128f870472322689f7580c10a2fe1a4e83
Noroval <122039895+Noroval@users.noreply.github.com>
2023-01-19 13:57:55 +0000

    Thu, Jan 19, 2023, 1:57 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -188,7 +188,10 @@ int find_min(void)
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
-    
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if 
+    }
     return false;
 }
 
