a8f7b2064bee52f03de67e9157e653b50da3b599
Noroval <122039895+Noroval@users.noreply.github.com>
2023-01-26 11:30:18 +0000

    Thu, Jan 26, 2023, 1:30 PM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -5,9 +5,9 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int row = 0; row < height; row++)
     {
-        for (int column = 0, column < width; column++)
+        for (int column = 0; column < width; column++)
         {
-            average = round((image[row][column]))
+            average = round((image[row][column].rgbtRed + image[row][column].rgbtGreen + image[row][column].rgbtBlue) / 3.0);
         }
     }
     return;
