83ce59403fa3deb256f828b876966d54c0e1150b
Noroval <122039895+Noroval@users.noreply.github.com>
2023-01-27 15:01:34 +0000

    Fri, Jan 27, 2023, 5:01 PM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -16,5 +16,18 @@ int main(int argc, char *argv[])
     return 1;
  }
 
- 
+ //open memory card
+ //repeat untill end of the card:
+      //read 512 bytes into a buffer
+      //if start of new JPEG
+        //if first JPEG
+        //mark first
+
+        //else
+        //close current, open new one
+
+      //else
+        //if already found JPEG
+
+  //close any remaining files
 }
