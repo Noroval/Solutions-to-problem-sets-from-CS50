e2e44fbb2325c83f5e6ddad09c801631c6e9647e
Noroval <122039895+Noroval@users.noreply.github.com>
2023-01-27 10:05:34 +0000

    Fri, Jan 27, 2023, 12:05 PM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -19,7 +19,14 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-
+    for (int row = 0; row < height; row++)
+    {
+        for (int column = 0; column < width; column++)
+        {
+            RGBTRIPLE pixcolor = image[row][column];
+            
+        }
+    }
     return;
 }
 
