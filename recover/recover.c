b936ef6d4ba245dbf525c3d3aa6ced95265a8d6b
Noroval <122039895+Noroval@users.noreply.github.com>
2023-01-30 11:51:01 +0000

    Mon, Jan 30, 2023, 1:51 PM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -28,8 +28,9 @@ int main(int argc, char *argv[])
 
  //repeat untill end of the card:
  while (fread(&buffer, 512, 1, file) == 1)
-
-
+ {
+  if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+ }
       //read 512 bytes into a buffer
       //if start of new JPEG
         //if first JPEG
