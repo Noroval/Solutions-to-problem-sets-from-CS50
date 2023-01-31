01bf3835d87eeaed5d70af00138f433ab7793cbe
Noroval <122039895+Noroval@users.noreply.github.com>
2023-01-31 19:13:31 +0000

    Tue, Jan 31, 2023, 9:13 PM +02:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -63,8 +63,9 @@ person *create_family(int generations)
         p->parents[0] = NULL;
         p->parents[1] = NULL;
 
-        // TODO: Randomly assign alleles
-
+        // Randomly assign alleles
+        p->alleles[0] = random_allele();
+        p->alleles[1] = random_allele();
     }
 
     // TODO: Return newly created person
