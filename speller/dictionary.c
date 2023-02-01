ad8fe74aaa7caccf348947e5587d561718c3af24
Noroval <122039895+Noroval@users.noreply.github.com>
2023-02-01 16:26:12 +0000

    Wed, Feb 1, 2023, 6:26 PM +02:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -101,7 +101,7 @@ bool load(const char *dictionary)
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
-    // TODO
+    // if dict is loaded, return it's size
     if (word_count > 0)
     {
         return word_count;
