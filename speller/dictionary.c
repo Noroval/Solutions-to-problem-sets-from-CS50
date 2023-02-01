9190f0d44b13e84e2c0d995a0159ab169829429d
Noroval <122039895+Noroval@users.noreply.github.com>
2023-02-01 13:06:56 +0000

    Wed, Feb 1, 2023, 3:06 PM +02:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -99,6 +99,15 @@ unsigned int size(void)
     return countW;
 }
 
+void freenode(node *n)
+{
+    if(n->next != NULL)
+    {
+        freenode(n->next);
+    }
+    free (n);
+}
+
 // Unloads dictionary from memory, returning true if successful, else false
 bool unload(void)
 {
