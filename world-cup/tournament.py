8871275f0a71e05f73e3034dd2e9c93afc37aec4
Noroval <122039895+Noroval@users.noreply.github.com>
2023-02-06 15:04:20 +0000

    Mon, Feb 6, 2023, 5:04 PM +02:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -17,7 +17,7 @@ def main():
     teams = []
     # TODO: Read teams into memory from file
     filename = sys.argv[1]
-    with open filename as file:
+    with open(filename) as file:
         reader = csv.DictReader(file)
         for team in reader:
             team["rating"] = int(team["rating"])
