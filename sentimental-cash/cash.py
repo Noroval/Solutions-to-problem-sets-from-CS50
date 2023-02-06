1f8807788b7db3fd443ac1cf80d4014cb1831c1f
Noroval <122039895+Noroval@users.noreply.github.com>
2023-02-06 19:46:31 +0000

    Mon, Feb 6, 2023, 9:46 PM +02:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -10,19 +10,19 @@ cents = round(dollars * 100)
 count = 0
 
 while cents >= 25:
-    cents - 25
+    cents = cents - 25
     count += 1
 
 while cents >= 10:
-    cents - 10
+    cents = cents - 10
     count += 1
 
 while cents >= 5:
-    cents - 5
+    cents = cents - 5
     count += 1
 
 while cents >= 1:
-    cents - 1
+    cents = cents - 1
     count += 1
 
 print(count)
