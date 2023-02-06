6f94ab0f2969dde8f5a2aa9d05af91aff5d1d48b
Noroval <122039895+Noroval@users.noreply.github.com>
2023-02-06 17:21:34 +0000

    Mon, Feb 6, 2023, 7:21 PM +02:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,5 +1,8 @@
 from cs50 import get_int
 
+#prints hashtag and spaces
+
+
 def main():
     height = get_height()
     for i in range(0, height, 1):
@@ -10,6 +13,7 @@ def main():
                 print("#", end="")
         print()
 
+#prompts the user for height and checks if it's between 1 to 8
 def get_height():
     while True:
         n = get_int("Height: ")
