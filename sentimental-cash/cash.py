417d25681130790c67c778560ecc44e22a60be3a
Noroval <122039895+Noroval@users.noreply.github.com>
2023-02-06 19:21:00 +0000

    Mon, Feb 6, 2023, 9:21 PM +02:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,9 +1,11 @@
 from cs50 import get_float
 
-def get_change():
+
+
+
+def get_dollars():
     while True:
         n = get_float("Change owed: ")
         if n > 0
             return n
 
-           
