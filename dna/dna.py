7b50f2771f65c9200a72d0e7f68e2a2a700aca1e
Noroval <122039895+Noroval@users.noreply.github.com>
2023-02-08 09:32:33 +0000

    Wed, Feb 8, 2023, 11:32 AM +02:00

--- dna/dna.py
+++ dna/dna.py
@@ -5,7 +5,7 @@ import sys
 def main():
 
     # Check for command-line usage
-    if len(argv) != 3:
+    if len(sys.argv) != 3:
         print("Usage: python dna.py data.csv sequence.txt")
 
     # TODO: Read database file into a variable
@@ -14,7 +14,6 @@ def main():
         reader = csv.DictReader(file)
         for row in reader:
             database.append(row)
-    print(database)
 
     # TODO: Read DNA sequence file into a variable
 
