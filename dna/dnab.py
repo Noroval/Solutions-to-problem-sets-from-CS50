fb1be32b22721648761f4f991ab5dec57e1f9464
Noroval <122039895+Noroval@users.noreply.github.com>
2023-02-09 13:28:06 +0000

    Thu, Feb 9, 2023, 3:28 PM +02:00

--- dna/dnab.py
+++
@@ -1,9 +0,0 @@
-import csv
-import sys
-
-database = []
-with open(sys.argv[1], "r") as file:
-    reader = csv.DictReader(file)
-    for row in reader:
-        database.append(row)
-print(database)
