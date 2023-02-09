e0e7f933b3bd3b84491c837efcbe4056824a4c12
Noroval <122039895+Noroval@users.noreply.github.com>
2023-02-09 13:02:41 +0000

    Thu, Feb 9, 2023, 3:02 PM +02:00

--- dnab.py
+++ dnab.py
@@ -2,8 +2,8 @@ import csv
 import sys
 
 database = []
-    with open(sys.argv[1], "r") as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            database.append(row)
+with open(sys.argv[1], "r") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        database.append(row)
 print(database)
