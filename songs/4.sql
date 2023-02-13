e183224b575aedc592ba812c465a8f58106f99ba
Noroval <122039895+Noroval@users.noreply.github.com>
2023-02-13 15:44:32 +0000

    Mon, Feb 13, 2023, 5:44 PM +02:00

--- songs/4.sql
+++ songs/4.sql
@@ -1,3 +1,3 @@
 -- write a SQL query that lists the names of any songs that have danceability, energy, and valence greater than 0.75
 
-SELECT name FROM songs 
+SELECT name FROM songs WHERE danceability, energy, valence 
