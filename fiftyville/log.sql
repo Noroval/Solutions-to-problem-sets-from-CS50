14efcbb8eb7cedfb0aa9eb445e81e305497823a9
Noroval <122039895+Noroval@users.noreply.github.com>
2023-02-21 15:15:35 +0000

    Tue, Feb 21, 2023, 5:15 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -7,5 +7,5 @@ FROM crime_scene_reports
 WHERE month = 7 AND day = 28
 AND street = 'Humphrey Street';
 
---
+-- Crime took place at 10.15am at Humprey Street bakery. 3 witnesses, all mentioned the bakery. Liitering took place at 16.36, no known witnesses.
 
