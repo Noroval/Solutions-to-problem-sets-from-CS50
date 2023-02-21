b9c44eb0fc5e2c42d726b1771e69e00a2b8674c5
Noroval <122039895+Noroval@users.noreply.github.com>
2023-02-21 15:10:54 +0000

    Tue, Feb 21, 2023, 5:10 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,3 +1,11 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
-Humphrey Street
+-- Looking into description to get more info about that day's crime
+
+SELECT description
+FROM crime_scene_reports
+WHERE month = 7 AND day = 28
+AND street = 'Humphrey Street';
+
+--
+
