6cc382733d52295005494e42556087de5989fa5d
Noroval <122039895+Noroval@users.noreply.github.com>
2023-02-21 16:10:01 +0000

    Tue, Feb 21, 2023, 6:10 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -27,5 +27,5 @@ AND transcript LIKE '%bakery%';
 
 SELECT name
 FROM people, bakery_security_logs
-WHERE 
-WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10;
+WHERE people.license_plate = bakery_security_logs.license_plate
+AND year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute 
