21996df0c6d77e2ea15f8de3ff2670fca49e62e5
Noroval <122039895+Noroval@users.noreply.github.com>
2023-02-22 14:48:02 +0000

    Wed, Feb 22, 2023, 4:48 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -86,4 +86,6 @@ AND passengers.passport_number = people.passport_number
 AND year = 2021 AND month = 7 AND day = 29 AND destination_airport_id = 4
 AND name IN ('Bruce', 'Diana', 'Robin', 'Philip');
 
--- Names are: Bruce, Luca
+-- Names are: Bruce
+-- Thief: Bruce
+-- As Bruce called Robin, she's accomplice
