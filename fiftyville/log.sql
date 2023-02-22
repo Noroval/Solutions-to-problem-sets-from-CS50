a6ab8517c03832ac4f73aa41609b7974fb27e770
Noroval <122039895+Noroval@users.noreply.github.com>
2023-02-22 09:10:14 +0000

    Wed, Feb 22, 2023, 11:10 AM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -51,15 +51,18 @@ AND atm_location = 'Leggett Street' AND transaction_type = 'withdraw';
 SELECT name
 FROM people, phone_calls
 WHERE phone_calls.caller = people.phone_number
-AND year = 2021 AND month = 7 AND day = 28 AND name IN ('Bruce', 'Diana', 'Iman', 'Luca');
+AND year = 2021 AND month = 7 AND day = 28 AND name IN ('Bruce', 'Diana', 'Iman', 'Luca')
+AND duration <= 60;
 
--- Names are: Bruce 4 times and Diana 1 time.
+-- Names are: Bruce, Diana
 
 -- Try to get person names which reiceved phone calls during that day
 
 SELECT name
 FROM people, phone_calls
 WHERE phone_calls.receiver = people.phone_number
-AND year = 2021 AND month = 7 AND day = 28 AND name IN ('Bruce', 'Diana', 'Iman', 'Luca');
+AND year = 2021 AND month = 7 AND day = 28 AND name IN ('Bruce', 'Diana', 'Iman', 'Luca')
+AND duration <= 60;
 
--- Names are: Luca 2 times and Diana 1 time.
+-- Names are: Luca
+
