7112abf738978f5c89c2ade54246de7cb5665e33
Noroval <122039895+Noroval@users.noreply.github.com>
2023-02-22 13:56:06 +0000

    Wed, Feb 22, 2023, 3:56 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -81,4 +81,6 @@ WHERE year = 2021 AND month = 7 AND day = 29;
 
 SELECT name
 FROM people, passengers, flights
-WHERE 
+WHERE flights.id = passengers.flight_id
+AND passengers.passport_number = people.passport_number
+AND 
